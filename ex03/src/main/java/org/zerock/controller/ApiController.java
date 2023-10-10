bdd0ef643ee41ca3dcfa6f801ff696f9bcc47ce4
NSJ <NSJ@DESKTOP-G9FKL7J>
2023-10-10 16:20:49 +0900

    REST로 list 보내기
    json형식으로

--- ex03/src/main/java/org/zerock/controller/ApiController.java
+++ ex03/src/main/java/org/zerock/controller/ApiController.java
@@ -1,13 +1,19 @@
 package org.zerock.controller;
 
+import java.util.List;
+
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.zerock.domain.BnoVO;
+import org.zerock.domain.BoardVO;
+import org.zerock.domain.Criteria;
 import org.zerock.domain.Total;
 import org.zerock.service.BoardService;
+import org.zerock.service.ReplyService;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
@@ -18,6 +24,7 @@ import lombok.extern.log4j.Log4j;
 public class ApiController {
 	
 	private BoardService service;
+	private ReplyService rpservice;
 	
 	@GetMapping(value = "/myapi/replybnocount", produces = MediaType.APPLICATION_JSON_VALUE)
 	public Long replyCount() {
@@ -40,7 +47,7 @@ public class ApiController {
 	}
 	//<Total><count>55</count></Total>    | 	{"count":55}
 	//<해당 클래스><해당 변수>
-	@GetMapping(value="/myapi/{num1}/{num2}",produces = {MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE,})
+	@GetMapping(value="/myapi/{num1}/{num2}",produces = {MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE})
 	public Total sum(@PathVariable("num1") Long num1,@PathVariable("num2") Long num2) {
 		Total a=new Total();
 		Long sum=0L;
@@ -51,4 +58,11 @@ public class ApiController {
 		return a;
 		
 	}
+	
+	@GetMapping(value="/myapi/board/{num1}/{num2}",produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<BoardVO> list(@PathVariable("num1") int pageNum,@PathVariable("num2") int amount) {
+		Criteria cri=new Criteria(pageNum,amount);		
+		return service.getList(cri);
+	}
+	
 }
